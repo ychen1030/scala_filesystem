9b8b2c5f7f974a7a6656adfb115855f48b8d59ca
Yingying Chen <ychen1030@berkeley.edu>
2020-06-08 00:59:06 -0700

    finalizing mkdir and add ls

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -8,6 +8,7 @@ trait Command {
 
 object Command {
   val MKDIR = "mkdir"
+  val LS = "ls"
 
   def emptyCommand: Command =
     (state: State) => state
@@ -23,7 +24,7 @@ object Command {
       if (tokens.length < 2) incompleteCommand(MKDIR)
       else new Mkdir(tokens(1))
     }
-//    else if ("")
+    else if (LS.equals(tokens(0))) new Ls
     else new UnknownCommand
   }
 
