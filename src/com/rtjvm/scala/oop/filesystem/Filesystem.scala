a9566e51cf5fb00b7e3513308c4c9253930432d2
Yingying Chen <ychen1030@berkeley.edu>
2020-06-12 22:48:30 -0700

    final polish

--- src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
+++ src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -8,12 +8,26 @@ import com.rtjvm.scala.oop.files.Directory
 object Filesystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
 
-  while (true) {
-    state.show()
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+  // [1, 2, 3, 4]
+  /*
+    0 (op) 1 => 1
+    1 (op) 2 => 3
+    3 (op) 3 => 6
+    6 (op) 4 => last value 10
+
+    List(1, 2, 3, 4).foldLeft(0)((x, y) => x + y)
+   */
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currState, newLine) => {
+    currState.show()
+    Command.from(newLine).apply(currState)
+  })
+
+//  var state = State(root, root)
+//  val scanner = new Scanner(System.in)
+//
+//  while (true) {
+//    state.show()
+//    val input = scanner.nextLine()
+//    state = Command.from(input).apply(state)
 }
